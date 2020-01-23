cd12db6046e230eeb77cebbd8fdb21c35a122af6
Amélie Houel <amelie.houel@hetic.net>
2020-01-24 00:25:40 +0100

    optimization css

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var button = document.querySelector('button');
-
+var cursor = document.getElementById('cursor');
+var position; 
+var lastPosition = 0;
+var links = document.querySelectorAll("a");
 
 button.addEventListener('click', function(){
   var name = document.getElementById('name').value;
@@ -7,3 +10,31 @@ button.addEventListener('click', function(){
   var msg = document.getElementById('msg').value;
   console.log('bonjour '+name+ ' ,votre mail : '+mail+', votre message : '+ msg);
 });
+
+
+window.addEventListener("scroll", scrollUpdate);
+
+function scrollUpdate(e) {
+  lastPosition = window.scrollY;
+  cursor.style.top = -15 + lastPosition + position + "px";
+}
+
+document.addEventListener("mousemove", mouseMove);
+
+function mouseMove(e) {
+  position = e.clientY;
+  cursor.style.left = -15 + e.clientX + "px";
+  cursor.style.top = -15 + e.clientY + lastPosition + "px";
+}
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("mouseover", function() {
+    cursor.classList.add("hover");
+  });
+}
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("mouseout", function() {
+    cursor.classList.remove("hover");
+  });
+}
