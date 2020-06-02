092e2c8696037977e0cb80ff22f2409826ed0ef2
AmelieHouel <amelie.houel@hetic.net>
2020-06-02 23:13:25 +0200

    modif post.php

--- post.php
+++ post.php
@@ -5,7 +5,7 @@
       $msg = 'Le message a été envoyé avec succès';
       $user = 'hoam4690';
       $password = '6KAxr3cL6*Qy';
-      $pdo = new PDO('mysql:host=localhost;dbname=hoam3649_portfolio; charset=utf8', $user, $password);
+      $pdo = new PDO('mysql:host=localhost;dbname=hoam4690_portfolio; charset=utf8', $user, $password);
 
     }
     catch (Exception $e)
@@ -13,9 +13,15 @@
       die('Erreur : ' . $e->getMessage());
     }
 
-    //Envoyer le message
-    $req = $pdo->prepare('INSERT INTO contact (username, mail, user_message, date_message) VALUES(?, ?, ?, NOW())');
-    $resultat = $req->execute(array($_POST['username'], $_POST['mail'], $_POST['user_message']));
+    //Si les variables existes
+    if(isset($_POST['username']) && isset($_POST['mail']) && isset($_POST['user_message'])) {
+
+    //Envoyer les données dans la BDD
+      $req = $pdo->prepare(' INSERT INTO contact (username, mail, user_message, date_message) VALUES(?, ?, ?, NOW())');
+      $res = $req->execute(array(htmlspecialchars($_POST['username']), htmlspecialchars($_POST['mail']), htmlspecialchars($_POST['user_message'])));
+      
+
+  }
 
     if (count($_POST)>0) echo "<p class=\"msg-user\">$msg</p>";
           
